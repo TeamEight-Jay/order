16ff876c3ba5219bc12537125df0b395e41936a5
sachinsekhar <sach.sekh.131997@gmail.com>
2019-01-19 17:13:37 +0530

    Linked Product and Order in the controller and DTO using Restful Template

--- src/main/java/com/teamfive/order/dto/ProductDTO.java
+++ src/main/java/com/teamfive/order/dto/ProductDTO.java
@@ -1,4 +1,49 @@
 package com.teamfive.order.dto;
 
 public class ProductDTO {
+    private String productId;
+    private String productName;
+    private String description;
+    private String keyFeatures;
+    private String imageSrc;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getKeyFeatures() {
+        return keyFeatures;
+    }
+
+    public void setKeyFeatures(String keyFeatures) {
+        this.keyFeatures = keyFeatures;
+    }
+
+    public String getImageSrc() {
+        return imageSrc;
+    }
+
+    public void setImageSrc(String imageSrc) {
+        this.imageSrc = imageSrc;
+    }
 }
